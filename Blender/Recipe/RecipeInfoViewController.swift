f834e86959334c2138e6e4d121c29fd312f58f9c
ayo1103 <ayo1103@gmail.com>
2016-02-19 06:56:56 +0800

    Add recipeInfoVC`

---
+++ Blender/Recipe/RecipeInfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeInfoViewController.swift
+//  Blender
+//
+//  Created by Bryan Lin on 2/19/16.
+//  Copyright © 2016 GiantCroissant. All rights reserved.
+//
+
+import UIKit
+
+class RecipeInfoViewController: UIViewController {
+
+}
