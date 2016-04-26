77d240101e7e375cf84722b6cc0f696c07479e95
ayo1103 <ayo1103@gmail.com>
2016-04-26 10:26:46 +0800

    Setup Product Cell UI

--- Blender/Recipe/CompanyInfo/ProductListViewController.swift
+++ Blender/Recipe/CompanyInfo/ProductListViewController.swift
@@ -19,4 +19,27 @@ class ProductListViewController: UITableViewController {
     products = ProductManager.sharedInstance.loadProductsByCategory(category)
     print("products = \(products)")
   }
+
+/*
+   @available(iOS 2.0, *)
+   public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+
+   // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+   // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+   @available(iOS 2.0, *)
+   public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+
+ */
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return products.count
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("recipeCell", forIndexPath: indexPath) as! RecipeCell
+    cell.recipe = recipes[indexPath.row]
+    return cell
+  }
+
 }
